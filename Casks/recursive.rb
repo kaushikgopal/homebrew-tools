7ffb0c68084acff855ff64e74f9c4f25676d73d6
kaush <1042037+kaushikgopal@users.noreply.github.com>
2021-10-17 19:43:34 -0700

    upload recursive font with sans

---
+++ Casks/recursive.rb
@@ -0,0 +1,18 @@
+cask "recursive" do
+  version "2.1.079"
+  sha256 "5347cb1da96ec1740892f6d7bd6dbc16cd04e6ef94670ebe60ba6d78c0eedfba"
+  url "https://github.com/kaushikgopal/recursive-code-config/releases/download/v2.1.079/recursive-2.1.079.zip"
+
+  name "Recursive Font Dark 02"
+  homepage "https://www.recursive.design/"
+
+  font "Recursive/RecMonoDarkCli02-Bold-1.079.ttf"
+  font "Recursive/RecMonoDarkCli02-BoldItalic-1.079.ttf"
+  font "Recursive/RecMonoDarkCli02-Italic-1.079.ttf"
+  font "Recursive/RecMonoDarkCli02-Regular-1.079.ttf"
+  font "Recursive/RecMonoDarkSans02-Bold-1.079.ttf"
+  font "Recursive/RecMonoDarkSans02-BoldItalic-1.079.ttf"
+  font "Recursive/RecMonoDarkSans02-Italic-1.079.ttf"
+  font "Recursive/RecMonoDarkSans02-Regular-1.079.ttf"
+
+end
