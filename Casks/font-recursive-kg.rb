f300ff658c096e5b101627cf1fe472f0e881ca37
kaush <1042037+kaushikgopal@users.noreply.github.com>
2023-02-28 08:59:18 -0800

    update: Recursive v10.1.085

--- Casks/font-recursive-kg.rb
+++ Casks/font-recursive-kg.rb
@@ -1,17 +1,17 @@
 cask "font-recursive-kg" do
-  version "7.1.085"
-  sha256 "cbe79563a2fc27d64f8c7f96914dfcfc62efd63d2fe07e23cd33399027c45a6e"
-  url "https://github.com/kaushikgopal/recursive-code-config/releases/download/v7.1.085/recursive-7.1.085.zip"
+  version "10.1.085"
+  sha256 "e272a5ff08fb77b8e5ec58b7f450c30b67b3085ea6ddf501f5a673b9ab6db474"
+  url "https://github.com/kaushikgopal/recursive-code-config/releases/download/v10.1.085/recursive-10.1.085.zip"
   name "Recursive Font KG"
   homepage "https://www.recursive.design/"
 
-  font "Recursive/RecursiveKG07-Bold-1.085.ttf"
-  font "Recursive/RecursiveKG07-BoldItalic-1.085.ttf"
-  font "Recursive/RecursiveKG07-Italic-1.085.ttf"
-  font "Recursive/RecursiveKG07-Medium-1.085.ttf"
-  font "Recursive/RecursiveKG07-MediumItalic-1.085.ttf"
-  font "Recursive/RecursiveKG07-Regular-1.085.ttf"
-  font "Recursive/RecursiveKG07-SemiBold-1.085.ttf"
-  font "Recursive/RecursiveKG07-SemiBoldItalic-1.085.ttf"
+  font "Recursive/RecursiveKG10-Bold-1.085.ttf"
+  font "Recursive/RecursiveKG10-BoldItalic-1.085.ttf"
+  font "Recursive/RecursiveKG10-Italic-1.085.ttf"
+  font "Recursive/RecursiveKG10-Medium-1.085.ttf"
+  font "Recursive/RecursiveKG10-MediumItalic-1.085.ttf"
+  font "Recursive/RecursiveKG10-Regular-1.085.ttf"
+  font "Recursive/RecursiveKG10-SemiBold-1.085.ttf"
+  font "Recursive/RecursiveKG10-SemiBoldItalic-1.085.ttf"
 
 end
