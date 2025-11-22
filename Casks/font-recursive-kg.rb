284d4f9c4c6cdf3fba1f8993e21e4a7c027d0ce6
kau.sh <1042037+kaushikgopal@users.noreply.github.com>
2025-11-21 20:42:40 -0800

    update: recursive kg 34

--- Casks/font-recursive-kg.rb
+++ Casks/font-recursive-kg.rb
@@ -1,7 +1,7 @@
 cask "font-recursive-kg" do
-  version "33.1.085"
-  sha256 "92d8d0e0df380031eb9bbb27c16b9f57cac8c7fc7a358ad9df75fe399c6230da"
-  url "https://github.com/kaushikgopal/recursive-code-config/releases/download/v33/recursive-33.1.085.zip"
+  version "34.1.085"
+  sha256 ""
+  url "https://github.com/kaushikgopal/recursive-code-config/releases/download/v34/recursive-34.1.085.zip"
   name "Recursive Font KG"
   homepage "https://www.recursive.design/"
   font "Recursive/RecursiveKG33-Regular-1.085.ttf"
